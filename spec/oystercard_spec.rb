e96f2eea3e34323b32c15e3eea1746f17b5e15df
Christopher <dagswebb@gmail.com>
2021-11-26 10:27:55 +0000

    Adding in Spec

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,7 +1,10 @@
 require "oystercard"
 
 RSpec.describe Oystercard do
-  let(:station){ double :station }
+  # let(:station){ double :station }
+  let(:entry_station) { double :station }
+  let(:exit_station) { double :station }
+
   describe "#balance" do 
     it { is_expected.to respond_to(:balance) }
 
@@ -25,48 +28,81 @@ RSpec.describe Oystercard do
   end
   
 
-  describe "#touch_in and #touch_out" do
+  describe "#touch_in" do
+
+    it { is_expected.to respond_to(:touch_in).with(1).argument } 
     
     it "can touch in" do
       subject.top_up(Oystercard::MINIMUM_AMOUNT)
-      subject.touch_in(station)
+      subject.touch_in(entry_station)
       expect(subject.in_journey).to eq true
-    end
-
-    it "can touch out" do
-      subject.top_up(Oystercard::MINIMUM_AMOUNT)
-      subject.touch_in(station)
-      subject.touch_out 
-      expect(subject.in_journey).to eq false
     end
 
     it 'raises an error if we touch in with funds less than minimum amount' do
       # allow(subject).to receive(:touch_in).and_return true
       # subject.balance
       minimum_amount = Oystercard::MINIMUM_AMOUNT
-      expect { subject.touch_in(station) }.to raise_error "Need minimum amount of £#{minimum_amount} to touch in"
+      expect { subject.touch_in(entry_station) }.to raise_error "Need minimum amount of £#{minimum_amount} to touch in"
     end 
+  end
+
+
+  describe "#touch_out" do
+
+    it "can touch out" do
+      subject.top_up(Oystercard::MINIMUM_AMOUNT)
+      subject.touch_in(entry_station)
+      subject.touch_out(exit_station)
+      expect(subject.in_journey).to eq false
+    end
 
     it "on touch out it will deduct from balance " do
       subject.top_up(Oystercard::MINIMUM_AMOUNT)
-      subject.touch_in(station)
-      expect { subject.touch_out }.to change{ subject.balance }.by(-Oystercard::MINIMUM_AMOUNT)
+      subject.touch_in(entry_station)
+      expect { subject.touch_out(exit_station) }.to change{ subject.balance }.by(-Oystercard::MINIMUM_AMOUNT)
     end
 
-  end
+    it { is_expected.to respond_to(:touch_out).with(1).argument } 
 
-  describe "#touch_in" do
-    it { is_expected.to respond_to(:touch_in).with(1).argument } 
   end
 
   describe "#entry_station" do
     
-    it { is_expected.to respond_to(:entry_station)} 
+    # it { is_expected.to respond_to(:entry_station)} 
 
     it 'remembers the entry station after touch in' do
       subject.top_up(Oystercard::MINIMUM_AMOUNT)
-      subject.touch_in(station)
-      expect(subject.entry_station).to eq station
+      subject.touch_in(entry_station)
+      expect(subject.entry_station).to eq entry_station
     end
   end
+
+  describe "#exit_station" do
+    
+    # it { is_expected.to respond_to(:exit_station)} 
+
+    it 'remembers the exit station after touch out' do
+      subject.top_up(Oystercard::MINIMUM_AMOUNT)
+      subject.touch_in(entry_station)
+      subject.touch_out(exit_station)
+      expect(subject.exit_station).to eq exit_station
+    end
+
+  # describe '#journey_list' do
+    
+  #   it 'stores list of journeys' do
+  #     subject.top_up(Oystercard::MINIMUM_AMOUNT)
+  #     station = subject.touch_in(station)
+  #     expect(subject.touch_in(station)).to eq station
+  # end
+
+  # it 'stores list of journeys' do
+  #   subject.top_up(Oystercard::MINIMUM_AMOUNT)
+  #   expect(subject.touch_in(station)).to eq :journey_list["station"]
+  # end
+
+  end
+
+
+
 end
